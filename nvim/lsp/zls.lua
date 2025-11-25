1e92d928bc5499ddd19f6b586eba04a80878cc1b
Anshuman Komawar <anshumankomawar@gmail.com>
2025-11-24 21:06:51 -0800

    added zls support

---
+++ nvim/lsp/zls.lua
@@ -0,0 +1,13 @@
+return {
+  cmd = { 'zls' },
+  filetypes = { 'zig', 'zir' },
+  root_markers = { 'zls.json', 'build.zig', '.git' },
+  capabilities = vim.lsp.protocol.make_client_capabilities(),
+  settings = {
+    zls = {
+      enable_build_on_save = true,
+      semantic_tokens = "partial",
+    }
+  },
+  workspace_required = false,
+}
--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,18 +1,52 @@
 vim.pack.add({
   "https://github.com/mason-org/mason.nvim",
-  "https://github.com/mfussenegger/nvim-jdtls"
+  -- java
+  "https://github.com/mfussenegger/nvim-jdtls",
+  -- zig
+  "https://github.com/ziglang/zig.vim",
 })
 
 require("mason").setup({})
 
 vim.lsp.enable({
   "lua_ls",
+  "zls",
 })
 
 vim.diagnostic.config({
   underline = true,
   virtual_text = false,
+  virtual_lines = true,
   signs = false,
   update_in_insert = false,
   severity_sort = true,
+})
+
+-- keymaps
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+
+-- ZLS 
+--
+vim.g.zig_fmt_parse_errors = 0
+vim.g.zig_fmt_autosave = 0
+
+-- zig autocommands
+vim.api.nvim_create_autocmd('BufWritePre',{
+  pattern = {"*.zig", "*.zon"},
+  callback = function(ev)
+    vim.lsp.buf.code_action({
+      context = { only = { "source.organizeImports" } },
+      apply = true,
+    })
+  end
+})
+
+vim.api.nvim_create_autocmd('BufWritePre',{
+  pattern = {"*.zig", "*.zon"},
+  callback = function(ev)
+    vim.lsp.buf.code_action({
+      context = { only = { "source.fixAll" } },
+      apply = true,
+    })
+  end
 })
